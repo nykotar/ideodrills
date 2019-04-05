62dda5eb79a49ac08019eaacce4ed960e985bc08
nykotar <popcornjuice@protonmail.com>
2019-04-04 22:11:38 -0300

    Adding style and language selection

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+var voiceSelect = document.querySelector('select')
+var voices = window.speechSynthesis.getVoices();
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Populate voice (language) list
+    for(i = 0; i < voices.length ; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].lang;
+        option.setAttribute("voice-index", i);
+        voiceSelect.appendChild(option);
+
+        if (voices[i].lang === "en-US") {
+            voiceSelect.selectedIndex = i;
+        }
+    }
+
+
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+
+
 var x;
 var ideograms = [];
 
@@ -14,6 +37,6 @@ function start() {
 function randomize() {
     var rand = Math.floor(Math.random() * ideograms.length);
     var utterThis = new SpeechSynthesisUtterance(ideograms[rand]);
-    utterThis.lang = "en-US";
+    utterThis.voice = voices[voiceSelect.selectedOptions[0].getAttribute("voice-index")]
     window.speechSynthesis.speak(utterThis);
 }
